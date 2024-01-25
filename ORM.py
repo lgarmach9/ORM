61a7135a2ed1a47d886e99ac0fcc6a8a02da3630
lgarmach9 <LGARCIAM814@alumnos.imf.com>
2024-01-25 16:26:28 +0100

    propiedad inventario

--- ORM.py
+++ ORM.py
@@ -19,6 +19,7 @@ class Persona:
         self.descanso = 100
         self.entidadenergia = ""
         self.entidaddescanso = ""
+        self.inventario = [1,2,3,4]
     def dibuja(self):
         self.entidad = lienzo.create_oval(
             self.posx-self.radio/2,
